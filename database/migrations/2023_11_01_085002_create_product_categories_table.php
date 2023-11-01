2a87e963f90b0102b453b2c7b4bfa02764fd731e
willy836 <wilsonkimiri836@gmail.com>
2023-11-01 20:30:55 +0300

    Default seed data for product categories

--- database/migrations/2023_11_01_085002_create_product_categories_table.php
+++ database/migrations/2023_11_01_085002_create_product_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -17,6 +18,15 @@ return new class extends Migration
             $table->string('slug');
             $table->timestamps();
         });
+
+        // Seed data
+        DB::table('product_categories')->insert([
+            ['name' => 'Phones', 'slug' => 'phones', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Computers', 'slug' => 'computers', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'TVs', 'slug' => 'tvs', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Appliances', 'slug' => 'appliances', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Fashion', 'slug' => 'fashion', 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 
     /**
